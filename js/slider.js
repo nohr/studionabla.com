9415f89d8f5710c161c7eef0e31c699a0ec4d9dc
Aite <aite@studionabla.com>
2020-02-12 08:29:00 -0500

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,154 @@
+function placeFeed(y) {
+    var x = document.getElementById(y);
+    if (y != "null") {
+        if (x.style.display == "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+        
+    } else {
+        return;
+    }
+}
+function checkFeed() {
+    var i;
+    for (i = 1; i < 11; i++) {
+        var x = document.getElementById(i);
+        if (x.style.display == "block") {
+            x.style.display = "none";
+        }
+    }
+}
+
+function placeText(y) {
+    var id = y.toString();
+    var text = document.getElementById("text");
+    var c1 = document.getElementById("c1");
+    var c2 = document.getElementById("c2");
+    var c3 = document.getElementById("c3");
+    var c4 = document.getElementById("c4");
+    if (text.style.content == id) {
+    }
+    
+    if (id == "0") {
+        text.innerHTML = text0;
+            c1.innerHTML = "";
+            c2.innerHTML = "";
+            c3.innerHTML = "";
+            c4.innerHTML = "";
+            text.style.content = "0";
+    } else {
+            if (id == "1") {
+                text.innerHTML = text1;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Nohri";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2019";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  3D Animation";
+                    text.style.content = "1";
+            } else if (id == "2") {
+                text.innerHTML = text2;
+                    c1.innerHTML = "Student Work";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2020";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  3D Animation";
+                    text.style.content = "2";
+            } else if (id == "3") {
+                text.innerHTML = text3;
+                    c1.innerHTML = "Student Work";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2020";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  3D Modelling";
+                    text.style.content = "3";
+            } else if (id == "4") {
+                text.innerHTML = text4;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Tidal";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2016";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe <br> MeLo-X";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Sound Design";
+                    text.style.content = "4";         
+            } else if (id == "5") {
+                text.innerHTML = text5;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Celine Clarke";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2019";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Sound Recording <br> Sound Design";
+                    text.style.content = "5";
+            } else if (id == "6") {
+                text.innerHTML = text6;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Nohri";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2019";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Web Development <br> Song Artwork";
+                    text.style.content = "6";
+            } else if (id == "7") {
+                text.innerHTML = text7;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Heat.wav";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2017";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Wordmark Design";
+                    text.style.content = "7";
+            } else if (id == "8") {
+                text.innerHTML = text8;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  NXGN";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2016";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Wordmark Design <br> Song Artwork";
+                    text.style.content = "8";
+            } else if (id == "9") {
+                text.innerHTML = text9;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Nohri";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2018";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span> A. Eboigbe <br> J. McGarrell <br> K. Castro";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span> Album Artwork";
+                    text.style.content = "9";
+            } else if (id == "10") {
+                text.innerHTML = text10;
+                    c1.innerHTML = "<span id ='capTitle'>Client |</span>  Mia Gladstone";
+                    c2.innerHTML = "<span id ='capTitle'>Year |</span>  2018";
+                    c3.innerHTML = "<span id ='capTitle'>Team |</span>  A. Eboigbe <br> Mia Gladstone";
+                    c4.innerHTML = "<span id ='capTitle'>Services |</span>  Song Artwork";
+                    text.style.content = "10";
+            } 
+    };  
+}
+
+function scrollWin(y) {
+    var scrollHeight = 0;
+    var slider = document.querySelector(".slider");
+    var x = 600;
+    var z = 50;
+    var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+    x = x * plusOrMinus;
+    z = z * plusOrMinus;
+    if (plusOrMinus === -1 || 1){
+        console.log(x);
+        console.log(z);
+        }
+    function recoil(){
+        slider.scrollBy({
+            top: z,
+            behavior: "smooth"
+        });
+    };
+    if (y.toString() == "0") {
+        if (scrollHeight >= 0){
+            slider.scrollBy({
+                top: x,
+                behavior: "smooth"
+            });
+            setTimeout(recoil, 400);
+            scrollHeight += (x+z);
+        }
+    } else {
+        return;
+    }
+}
+
+
+function myFunction(y) {
+    checkFeed();
+    placeFeed(y);
+    placeText(y);
+    scrollWin(y);
+}
