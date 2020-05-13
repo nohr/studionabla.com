83616c1a196844b619163974919e4be902503666
Aite <aite@studionabla.com>
2020-05-13 05:01:32 -0400

    my services

--- js/text.js
+++ js/text.js
@@ -1,7 +1,7 @@
 if (window.matchMedia('only screen and (min-width: 1200px)').matches) {
-    var text0 = "Studio Nabla is a multi-disciplinary art and design studio founded by Aite Eboigbe in 2017. <br>To view a work, scroll through the sidebar to the left, select a work, and then scroll vertically. <br> <br> <span> If you would like to inquire about our services, feel free to contact me at <a href='mailto:info@studionabla.com'>info@studionabla.com</a>.";
+    var text0 = "Studio Nabla is a multi-disciplinary art and design studio founded by Aite Eboigbe in 2017. <br>To view a work, scroll through the sidebar to the left, select a work, and then scroll vertically. <br> <br> <span> If you would like to inquire about my services, feel free to contact me at <a href='mailto:info@studionabla.com'>info@studionabla.com</a>.";
 } else if (window.matchMedia('only screen and (max-width: 1200px)').matches) {
-    var text0 = "Studio Nabla is a multi-disciplinary art and design studio founded by Aite Eboigbe in 2017. <br>To view a work, scroll through the sidebar above, select a work, and then scroll horizontally. <br> <br> <span> If you would like to inquire about our services, feel free to contact me at <a href='mailto:info@studionabla.com'>info@studionabla.com</a>.";
+    var text0 = "Studio Nabla is a multi-disciplinary art and design studio founded by Aite Eboigbe in 2017. <br>To view a work, scroll through the sidebar above, select a work, and then scroll horizontally. <br> <br> <span> If you would like to inquire about my services, feel free to contact me at <a href='mailto:info@studionabla.com'>info@studionabla.com</a>.";
 }
 var text1 = "";
 var text2 = "";
